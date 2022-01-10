1b9b0a2a0d0b1bdc1b71aed94810c809206c982a
Muhammad Usman <prousman741@gmail.com>
2022-01-10 23:43:28 +0500

    initial layout added for the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Body from './layout/Body'
+import Footer from './layout/Footer'
+import Header from './layout/Header'
 
 const App = () => {
   return (
-    <div>
-      Portfolio
-    </div>
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   )
 }
 
---
+++ src/layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <p>This is the Header</p>
+        </div>
+    )
+}
+
+export default Header
